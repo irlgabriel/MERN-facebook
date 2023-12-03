ce5d0eb7429c4b46bbc828912dff4707e515e460
Gabriel Radu <gabriel.radu@sensidev.net>
2023-12-03 04:30:41 +0200

    remove commented code

--- server/app.ts
+++ server/app.ts
@@ -52,27 +52,27 @@ app.use("/posts/:post_id/comments", commentsRouter);
 app.use("/notifications", notificationsRouter);
 app.use("/friend_requests", friendRequestsRouter);
 
-// if (process.env.NODE_ENV !== "development") {
-//   app.get("*", function (request, response) {
-//     response.sendFile(path.resolve(__dirname, "client/build", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV !== "development") {
+  app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+  });
+}
 
-// // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
 
-// // error handler
-// app.use((err, req, res, next) => {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   console.log(err);
-//   res.json(err);
-// });
+  // render the error page
+  res.status(err.status || 500);
+  console.log(err);
+  res.json(err);
+});
 
 export default app;
